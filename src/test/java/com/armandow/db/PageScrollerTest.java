8fa5bbc70091877e1c402b85325e4eea2ca5cfb5
Armando Castillo <armandowg@gmail.com>
2024-08-06 12:10:20 -0600

    bump dependencies
    - org.xerial:sqlite-jdbc from 3.46.0.0 to 3.46.0.1

--- src/test/java/com/armandow/db/PageScrollerTest.java
+++ src/test/java/com/armandow/db/PageScrollerTest.java
@@ -13,12 +13,12 @@ public class PageScrollerTest {
         for (int i = 1; i <= 35; i++) {
             var pageScroller = PageScroller.create(12, 1734, 50, i);
             assertNotNull(pageScroller);
-            log.trace(pageScroller.size() + " :: " +pageScroller);
+            log.trace("{} :: {}", pageScroller.size(), pageScroller);
             assertTrue(pageScroller.size() >=12 && pageScroller.size() <= 13);
 
             var pageScroller2 = PageScroller.create(7, 3, 10, i);
             assertNotNull(pageScroller2);
-            log.trace(pageScroller2.size() + " :: " +pageScroller2);
+            log.trace("{} :: {}", pageScroller2.size(), pageScroller2);
             assertTrue(pageScroller2.size() <= 1);
         }
     }
